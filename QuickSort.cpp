6246049ee1a33d82f807b40c6eccfc4c2dbf1242
Shahin505 <75542645+Shahin505@users.noreply.github.com>
2023-09-16 19:36:13 +0600

    Update QuickSort.cpp

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -1,100 +1,56 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <stdio.h>
 
-// Merges two subarrays of array[].
-// First subarray is arr[begin..mid]
-// Second subarray is arr[mid+1..end]
-void merge(int array[], int const left, int const mid,
-		int const right)
+void swap(int* a, int* b)
 {
-	int const subArrayOne = mid - left + 1;
-	int const subArrayTwo = right - mid;
+	int t = *a;
+	*a = *b;
+	*b = t;
+}
 
-	// Create temp arrays
-	auto *leftArray = new int[subArrayOne],
-		*rightArray = new int[subArrayTwo];
+int partition(int arr[], int low, int high)
+{
 
-	// Copy data to temp arrays leftArray[] and rightArray[]
-	for (auto i = 0; i < subArrayOne; i++)
-		leftArray[i] = array[left + i];
-	for (auto j = 0; j < subArrayTwo; j++)
-		rightArray[j] = array[mid + 1 + j];
+	int pivot = arr[high];
 
-	auto indexOfSubArrayOne = 0, indexOfSubArrayTwo = 0;
-	int indexOfMergedArray = left;
 
-	// Merge the temp arrays back into array[left..right]
-	while (indexOfSubArrayOne < subArrayOne
-		&& indexOfSubArrayTwo < subArrayTwo) {
-		if (leftArray[indexOfSubArrayOne]
-			<= rightArray[indexOfSubArrayTwo]) {
-			array[indexOfMergedArray]
-				= leftArray[indexOfSubArrayOne];
-			indexOfSubArrayOne++;
+	int i = (low - 1);
+
+	for (int j = low; j <= high - 1; j++) {
+
+
+		if (arr[j] < pivot) {
+
+
+			i++;
+			swap(&arr[i], &arr[j]);
 		}
-		else {
-			array[indexOfMergedArray]
-				= rightArray[indexOfSubArrayTwo];
-			indexOfSubArrayTwo++;
-		}
-		indexOfMergedArray++;
 	}
-
-	// Copy the remaining elements of
-	// left[], if there are any
-	while (indexOfSubArrayOne < subArrayOne) {
-		array[indexOfMergedArray]
-			= leftArray[indexOfSubArrayOne];
-		indexOfSubArrayOne++;
-		indexOfMergedArray++;
-	}
-
-	// Copy the remaining elements of
-	// right[], if there are any
-	while (indexOfSubArrayTwo < subArrayTwo) {
-		array[indexOfMergedArray]
-			= rightArray[indexOfSubArrayTwo];
-		indexOfSubArrayTwo++;
-		indexOfMergedArray++;
-	}
-	delete[] leftArray;
-	delete[] rightArray;
+	swap(&arr[i + 1], &arr[high]);
+	return (i + 1);
 }
 
-// begin is for left index and end is right index
-// of the sub-array of arr to be sorted
-void mergeSort(int array[], int const begin, int const end)
+void quickSort(int arr[], int low, int high)
 {
-	if (begin >= end)
-		return;
+	if (low < high) {
 
-	int mid = begin + (end - begin) / 2;
-	mergeSort(array, begin, mid);
-	mergeSort(array, mid + 1, end);
-	merge(array, begin, mid, end);
+
+		int pi = partition(arr, low, high);
+
+
+		quickSort(arr, low, pi - 1);
+		quickSort(arr, pi + 1, high);
+	}
 }
 
-// UTILITY FUNCTIONS
-// Function to print an array
-void printArray(int A[], int size)
-{
-	for (int i = 0; i < size; i++)
-		cout << A[i] << " ";
-	cout << endl;
-}
-
-// Driver code
 int main()
 {
-	int arr[] = { 12, 11, 13, 5, 6, 7 };
-	int arr_size = sizeof(arr) / sizeof(arr[0]);
+	int arr[] = { 10, 7, 8, 9, 1, 5 };
+	int N = sizeof(arr) / sizeof(arr[0]);
 
-	cout << "Given array is \n";
-	printArray(arr, arr_size);
 
-	mergeSort(arr, 0, arr_size - 1);
-
-	cout << "\nSorted array is \n";
-	printArray(arr, arr_size);
+	quickSort(arr, 0, N - 1);
+	printf("Sorted array: \n");
+	for (int i = 0; i < N; i++)
+		printf("%d ", arr[i]);
 	return 0;
 }
